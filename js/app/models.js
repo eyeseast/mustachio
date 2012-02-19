a7300d557a302e22509e47553da217c6eabb2e3b
chrisamico <eyeseast@gmail.com>
2012-02-18 20:09:31 -0500

    Pre-formatting tweets

--- js/app/models.js
+++ js/app/models.js
@@ -9,10 +9,26 @@ var Template = Backbone.Model.extend({
         }
     },
     
-    localStorage: new Store('template')
+    localStorage: new Store('template'),
+    
+    initialize: function(attributes, options) {
+        this.on('change:code', function(model, code, attributes) {
+            if (!code.trim()) {
+                model.set({ code: model.defaults()['code']});
+            }
+        });
+    }
     
 });
 
 var Tweet = Backbone.Model.extend({
     
+    initialize: function(attributes, options) {
+        var changes = {};
+        changes.text = twttr.txt.autoLink(attributes.text);
+        if (attributes.rewteeted_status) {
+            changes.retweeted_status.text = twttr.txt.autoLink(changes.retweeted_status.text);
+        }
+        this.set(changes);
+    }
 });
